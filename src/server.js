93419841e14e35a31ad4c28307d973b490590b73
정우진 <jeong-ujin@jeong-ujin-ui-MacBook-Pro.local>
2020-04-23 04:28:46 +0900

    #1.1 Server Setup

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
 import express from "express";
+import socketIO from "socket.io";
+
+const PORT = 4000;
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "./src/views");
+app.use(express.static("./src/static"));
+
+app.get("/", (req, res) => res.render("home"));
+
+const handleListening = () => {
+  console.log(`✅Server running: httl://localhost:${PORT}`);
+};
+
+app.listen(PORT, handleListening);
