d39c645bffd313bd982df3a7354d166f9bc85d2b
정우진 <jeong-ujin@jeong-ujin-ui-MacBook-Pro.local>
2020-04-23 05:28:36 +0900

    2.3 Chat with SocketIO

--- src/server.js
+++ src/server.js
@@ -22,5 +22,13 @@ const server = app.listen(PORT, handleListening);
 const io = socketIO(server);
 
 io.on("connection", (socket) => {
-  socket.on("helloGuys", () => console.log("the client said hello"));
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Anon"
+    });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
 });
--- src/static/index.js
+++ src/static/index.js
@@ -1,4 +1,18 @@
 // eslint-disable-next-line no-undef
 const socket = io("/");
 
-setTimeout(() => socket.emit("helloGuys"), 4000);
+function sendMessage(message) {
+  socket.emit("newMessage", { message });
+  console.log(`You: ${message}`);
+}
+
+function setNickname(nickname) {
+  socket.emit("setNickname", { nickname });
+}
+
+function handleMessageNotif(data) {
+  const { message, nickname } = data;
+  console.log(`${nickname}:${message}`);
+}
+
+socket.on("messageNotif", handleMessageNotif);
